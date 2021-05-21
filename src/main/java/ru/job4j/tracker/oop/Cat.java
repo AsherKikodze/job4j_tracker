144bdce41d4f1e56907fb63852409485bd269085
AsherKikodze <asher.kikodze@gmail.com>
2021-05-21 08:47:55 +0300

    1.2.1. Классы и объекты 1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        Cat black = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        black.eat("fish");
+        black.giveNick("Black");
+        gav.show();
+        black.show();
+
+
+    }
+}
