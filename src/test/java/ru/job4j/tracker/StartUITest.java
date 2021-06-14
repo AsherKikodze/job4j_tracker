5f31ca23fedc1421c4f9d4967469c57a8614aa17
AsherKikodze <asher.kikodze@gmail.com>
2021-06-14 09:56:08 +0300

    1.2.4. Полиморфизм 9.1 Зависимость от System.out [#33568 #167438]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -57,4 +57,21 @@ public class StartUITest {
         //assertThat(tracker.findById(item.getId()), is(nullValue()));
         assertNull(tracker.findById(item.getId()));
     }
+
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Exit" + System.lineSeparator()
+        ));
+    }
 }
